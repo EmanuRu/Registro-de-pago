6361fecbd28b2ec373bc58b52cdc4320ebb79036
emanu <emanu@DESKTOP-QD01529>
2023-09-13 11:16:20 -0300

    Agregar archivos de proyecto.

---
+++ Registro de pago/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Registro_de_pago
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbMonto = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnEfectivo = new System.Windows.Forms.Button();
+            this.btnTarjeta = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblPagos = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Monto";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(136, 103);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(127, 20);
+            this.tbNombre.TabIndex = 2;
+            // 
+            // tbMonto
+            // 
+            this.tbMonto.Location = new System.Drawing.Point(136, 166);
+            this.tbMonto.Name = "tbMonto";
+            this.tbMonto.Size = new System.Drawing.Size(127, 20);
+            this.tbMonto.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.lblPagos);
+            this.panel1.Location = new System.Drawing.Point(514, 103);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(224, 170);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnEfectivo
+            // 
+            this.btnEfectivo.Location = new System.Drawing.Point(105, 240);
+            this.btnEfectivo.Name = "btnEfectivo";
+            this.btnEfectivo.Size = new System.Drawing.Size(75, 23);
+            this.btnEfectivo.TabIndex = 5;
+            this.btnEfectivo.Text = "Efectivo";
+            this.btnEfectivo.UseVisualStyleBackColor = true;
+            this.btnEfectivo.Click += new System.EventHandler(this.btnEfectivo_Click);
+            // 
+            // btnTarjeta
+            // 
+            this.btnTarjeta.Location = new System.Drawing.Point(211, 240);
+            this.btnTarjeta.Name = "btnTarjeta";
+            this.btnTarjeta.Size = new System.Drawing.Size(75, 23);
+            this.btnTarjeta.TabIndex = 6;
+            this.btnTarjeta.Text = "Tarjeta";
+            this.btnTarjeta.UseVisualStyleBackColor = true;
+            this.btnTarjeta.Click += new System.EventHandler(this.btnTarjeta_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(358, 166);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(34, 13);
+            this.lblTotal.TabIndex = 7;
+            this.lblTotal.Text = "Total:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(400, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "! Tenga en cuenta que con el pago en efectivo se le realizara un descunto del 10%" +
+    "";
+            // 
+            // lblPagos
+            // 
+            this.lblPagos.AutoSize = true;
+            this.lblPagos.Location = new System.Drawing.Point(4, 17);
+            this.lblPagos.Name = "lblPagos";
+            this.lblPagos.Size = new System.Drawing.Size(0, 13);
+            this.lblPagos.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(352, 204);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnTarjeta);
+            this.Controls.Add(this.btnEfectivo);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tbMonto);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cobro";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbMonto;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnEfectivo;
+        private System.Windows.Forms.Button btnTarjeta;
+        private System.Windows.Forms.Label lblPagos;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
+
---
+++ Registro de pago/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registro_de_pago
+{
+    public partial class Form1 : Form
+    {
+        String Nombre;
+        int Monto;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CargarEfectivo() {//este metodo toma el nombre y precio ingresado, a este ultimo le
+                                      //realiza un descuento de 10%
+            if (!tbNombre.Text.Equals("") && !tbMonto.Text.Equals(""))
+            {//corroboro que los textbox no esten vacios
+                Nombre = tbNombre.Text;//guardo el nombre
+                int valorIngresado = int.Parse(tbMonto.Text);//convierte el textbo a entero
+                int descuento = (valorIngresado * 10) / 100;//guardo lo que se va a descontar
+                Monto = valorIngresado - descuento;//hago el descuento
+                String total = "Total: " + Monto.ToString();//convierto todo a un String
+                lblTotal.Text = total;//lo muestro en la label
+            }
+        }
+
+        public void CargarTarjeta() {
+            if (!tbNombre.Text.Equals("") && !tbMonto.Text.Equals(""))
+            {//corroboro que los textbox no esten vacios
+                Nombre = tbNombre.Text;//guardo el nombre
+                Monto = int.Parse(tbMonto.Text);//convierte el textbox a entero
+                String total = "Total: " + Monto.ToString();//convierto todo a un String
+                lblTotal.Text = total;//lo muestro en la label
+            }
+        }
+
+        public void Agregar() {
+            if (!tbNombre.Text.Equals("")&&!tbMonto.Text.Equals(""))
+            { //corroboro que los textbox no esten vacios
+                lblPagos.Text += Nombre+"  "+Monto+ "\n";//guardo el nombre y pago en un label que hace de lista
+                tbNombre.Text = "";//limpio los demas elementos
+                tbMonto.Text = "";
+                lblTotal.Text = "Total:";
+                Nombre = "";
+                Monto = 0;
+            }
+        }
+
+        private void btnEfectivo_Click(object sender, EventArgs e)
+        {
+            CargarEfectivo();
+        }
+
+        private void btnTarjeta_Click(object sender, EventArgs e)
+        {
+            CargarTarjeta();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Agregar();
+        }
+    }
+}
